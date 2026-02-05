da672826427807d39f14c6002c68cf1073d75884
LeonardoSantosBR <leosantosmelo07@gmail.com>
2026-02-05 12:33:51 -0300

    feat: token bearer sendo passado

--- src/api/orders/orders.ts
+++ src/api/orders/orders.ts
@@ -1,12 +1,18 @@
 import type { IcreateOrders } from "../../types/orders/Icreate-orders";
 import { api } from "../api";
+const token = localStorage.getItem("token");
 
 export function createOrders(data: IcreateOrders) {
-  return api.post("/orders", data);
+  return api.post("/orders", data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 }
 
 export function getOrders(offset: number, pageSize: number) {
-  return api.get(`/orders`, { params: { page: offset, limit: pageSize } });
+  return api.get(`/orders`, {
+    headers: { Authorization: `Bearer ${token}` },
+    params: { page: offset, limit: pageSize },
+  });
 }
 
 export function updateOrders(
@@ -17,9 +23,13 @@ export function updateOrders(
     status: string;
   }
 ) {
-  return api.put(`/orders/${id}`, data);
+  return api.put(`/orders/${id}`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 }
 
 export function deleteOrders(id: string) {
-  return api.delete(`/orders/${id}`);
+  return api.delete(`/orders/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 }
